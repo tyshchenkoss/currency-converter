86c9edad987a298c26b2e68c706799925b076bb4
Serhii Tyshchenko <tyshchenko.sergey21@gmail.com>
2024-11-05 19:15:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [result, setResult] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function requestConverstion() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+        );
+        const requestResult = await res.json();
+        console.log(requestResult.rates);
+        setResult(requestResult.amount * requestResult.rates[to]);
+        setIsLoading(false);
+      } catch (err) {
+        setResult(null);
+        console.log(err);
+      }
+    }
+    if (from === to) {
+      return setResult(amount);
+    }
+
+    if (from && to && amount) {
+      requestConverstion();
+    }
+  }, [from, to, amount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setAmount(e.target.value)}
+        value={amount}
+        disabled={isLoading}
+      />
+      <select
+        onChange={(e) => setFrom(e.target.value)}
+        value={from}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        onChange={(e) => setTo(e.target.value)}
+        value={to}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{result ? result : "OUTPUT"}</p>
     </div>
   );
 }
